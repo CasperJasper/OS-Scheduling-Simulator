6411bc19f688d34de70659e595f7e9b072b61452
Kemier F. FRANCIS <115446979+CasperJasper@users.noreply.github.com>
2025-11-25 17:38:42 -0400

    Add docstring and update __all__ in __init__.py
    Added module docstring and updated exports.

--- src/scheduling/__init__.py
+++ src/scheduling/__init__.py
@@ -1,1 +1,23 @@
+"""
+Scheduling package for OS Scheduling Simulator
+Contains offloading strategies and scheduler implementations
+"""
 
+from .offload_strategy import (
+    OffloadStrategy, 
+    StaticOffloadStrategy, 
+    IntelligentOffloadStrategy,
+    static_policy,
+    heuristic_policy
+)
+
+from .list_scheduler import ListScheduler
+
+__all__ = [
+    'OffloadStrategy',
+    'StaticOffloadStrategy', 
+    'IntelligentOffloadStrategy',
+    'static_policy',
+    'heuristic_policy',
+    'ListScheduler'
+]
