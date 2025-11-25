22e9a3f76b83d386e93ba101275ff0201df1053e
Kemier F. FRANCIS <115446979+CasperJasper@users.noreply.github.com>
2025-11-25 17:38:12 -0400

    Document models package and set __all__
    Added docstring to models package and defined exports.

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,10 @@
+"""
+Models package for OS Scheduling Simulator
+Contains core data structures: Task, Server, Device
+"""
 
+from .task import Task
+from .server import Server
+from .device import Device
+
+__all__ = ['Task', 'Server', 'Device']
